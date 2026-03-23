91d4519267fd53b698e96e08be6a319e81629d4c
North Star House <media@thenorthstarhouse.org>
2026-03-22 22:34:27 -0700

    v311: Three-box onboarding layout: person list + interactive pipeline + year in review

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-var CACHE = 'nsh-v310';
+var CACHE = 'nsh-v311';
 var STATIC = [
   '/Portal/',
   '/Portal/index.html',
-  '/Portal/assets/app.js?v=310',
+  '/Portal/assets/app.js?v=311',
   '/Portal/assets/logo.png',
   '/Portal/favicon.svg',
 ];
