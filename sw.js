82ef38132637732a033e3cfd629e3ac4f10b7d5e
North Star House <media@thenorthstarhouse.org>
2026-03-23 08:42:54 -0700

    v318: Volunteer picker on reimbursement form, matching box headers

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-var CACHE = 'nsh-v317';
+var CACHE = 'nsh-v318';
 var STATIC = [
   '/Portal/',
   '/Portal/index.html',
-  '/Portal/assets/app.js?v=317',
+  '/Portal/assets/app.js?v=318',
   '/Portal/assets/logo.png',
   '/Portal/favicon.svg',
 ];
