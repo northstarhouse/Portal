feb91f3b1a6f02973f1eb69788b03e939d78bd96
North Star House <media@thenorthstarhouse.org>
2026-03-22 22:37:34 -0700

    v312: Color person cards and pipeline chips by area of interest

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-var CACHE = 'nsh-v311';
+var CACHE = 'nsh-v312';
 var STATIC = [
   '/Portal/',
   '/Portal/index.html',
-  '/Portal/assets/app.js?v=311',
+  '/Portal/assets/app.js?v=312',
   '/Portal/assets/logo.png',
   '/Portal/favicon.svg',
 ];
